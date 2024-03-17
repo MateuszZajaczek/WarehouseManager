3bfa45843e52dc44ac6876a69f8dadca1a440515
Mateusz <matiz588@gmail.com>
2024-03-17 12:32:44 +0100

    DataContext add

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseManager.Entities;
+
+namespace WarehouseManager.Data;
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+

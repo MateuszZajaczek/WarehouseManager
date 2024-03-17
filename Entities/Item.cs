a8ea70c7a3608099c27275a9409d9eb9ee48fc55
Mateusz <matiz588@gmail.com>
2024-03-17 12:05:36 +0100

    No comment

---
+++ Entities/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace WarehouseManager.Entities
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        // Add other properties that represent columns in the table
+    }
+
+}

b3e4633553ecaec4c72122d640df0859dc35e389
Mateusz <matiz588@gmail.com>
2024-03-18 13:03:53 +0100

    ItemController list & specific IDitem added

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseManager.Data;
+using WarehouseManager.Entities;
+
+namespace WarehouseManager.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")] // 
+    public class ItemController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ItemController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Item>> GetItem() // List of items
+        {
+            var items = _context.Items;
+            return items;
+        }
+
+        [HttpGet("{id}")] // Specific item
+
+        public ActionResult<Item> GetItem(int id)
+        {
+            var item = _context.Items.Find(id);
+
+            return item;
+        }
+    }
+}
